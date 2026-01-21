311455dd4b334f6f348edeb61dfa37fcc405a46c
juanqp07 <odioelmando2@gmail.com>
2026-01-21 20:06:32 +0100

    Añadido gcc y cmake

--- hosts/portatil/configuration.nix
+++ hosts/portatil/configuration.nix
@@ -36,6 +36,6 @@
   environment.systemPackages = with pkgs; [
   powertop         # Para ver qué gasta batería
   libinput-gestures # Gestos en el touchpad
-  go antigravity
+  go antigravity gcc cmake
 ];
 }
