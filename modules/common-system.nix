c2547ab3658a4273fa0d857a7b9547f7cf4f60a9
juanqp07 <odioelmando2@gmail.com>
2026-01-23 19:19:57 +0100

    Añadido idiomas al archivo general

--- modules/common-system.nix
+++ modules/common-system.nix
@@ -5,49 +5,73 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Actualización automática del sistema (Opcional)
-  # system.autoUpgrade = {
-  #   enable = true;
-  #   flake = inputs.self.outPath;
-  #   flags = [
-  #     "--update-input" "nixpkgs"
-  #     "-L" # print build logs
-  #   ];
-  #   dates = "04:00";
-  #   randomizedDelaySec = "45min";
-  # };
-
-  # --- RED Y LOCALE ---
+  # --- RED Y LOCALIZACIÓN ---
   networking.networkmanager.enable = true;
   time.timeZone = "Europe/Madrid";
-  i18n.defaultLocale = "es_ES.UTF-8";
   
-  # --- USUARIO ---
+  i18n.defaultLocale = "es_ES.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_ES.UTF-8";
+    LC_IDENTIFICATION = "es_ES.UTF-8";
+    LC_MEASUREMENT = "es_ES.UTF-8";
+    LC_MONETARY = "es_ES.UTF-8";
+    LC_NAME = "es_ES.UTF-8";
+    LC_NUMERIC = "es_ES.UTF-8";
+    LC_PAPER = "es_ES.UTF-8";
+    LC_TELEPHONE = "es_ES.UTF-8";
+    LC_TIME = "es_ES.UTF-8";
+  };
+
+  # --- TECLADO ---
+  # Configuración para la consola (TTY)
+  console.keyMap = lib.mkDefault "es";
+
+  # Configuración para el entorno gráfico (X11/Wayland)
+  services.xserver.xkb = {
+    layout = lib.mkDefault "es";
+    variant = lib.mkDefault "";
+  };
+
+  # --- SONIDO (Pipewire) ---
+  # Desactivamos PulseAudio estándar para usar Pipewire
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # --- SERVICIOS BÁSICOS ---
+  services.printing.enable = lib.mkDefault true; # Impresoras
+  services.netbird.enable = true;
+
+  # --- CONFIGURACIÓN DE USUARIO ---
   users.users.juan = {
     isNormalUser = true;
     description = "juan";
     shell = pkgs.fish;
-    extraGroups = [ "networkmanager" "wheel" ]; 
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" ]; 
   };
   programs.fish.enable = true;
 
   # --- NIX SETTINGS ---
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-nix.gc = {
+  
+  nix.gc = {
     automatic = true;
     dates = "weekly";
-    # Usamos mkDefault para que sea un "si nadie dice lo contrario, usa 7d"
     options = lib.mkDefault "--delete-older-than 7d"; 
   };
   nix.settings.auto-optimise-store = true;
 
   # --- PAQUETES DEL SISTEMA --- 
   environment.systemPackages = with pkgs; [
-    git wget curl btop htop fastfetch vim pciutils lshw
+    # Herramientas esenciales
+    git wget curl vim btop htop fastfetch pciutils lshw usbutils
   ];
 
-  services.netbird.enable = true;
-  
   system.stateVersion = "25.11";
 }
