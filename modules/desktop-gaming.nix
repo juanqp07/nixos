f2d572c8a7997208a6015249dfaafe7e0f69e353
juanqp07 <odioelmando2@gmail.com>
2026-01-24 16:06:32 +0100

    Añadido stremio

--- modules/desktop-gaming.nix
+++ modules/desktop-gaming.nix
@@ -44,7 +44,7 @@
     kdePackages.kate gedit
     distrobox vscode    
     netbird-ui
-    vlc mpv
+    vlc mpv stremio
     windsurf podman-compose
     gearlever cloudflare-warp
     rustdesk-flutter
