9dc58558da2865cd36a3bffb8d332f061edae1ef
JuanQ <87898162+juanqp07@users.noreply.github.com>
2026-01-27 14:19:11 +0100

    Add obs-studio to desktop gaming packages

--- modules/desktop-gaming.nix
+++ modules/desktop-gaming.nix
@@ -68,7 +68,7 @@
     protonplus supersonic-wayland
     antigravity python315 kdePackages.kcalc
     heroic rustdesk-flutter
-    go lm_sensors
+    go lm_sensors obs-studio
   ];
 
   # --- FLATPAK ---
