90a43c92624ca6da645463882df4ac1c849cf8a5
juanqp07 <odioelmando2@gmail.com>
2026-01-30 08:47:49 +0100

    feat: add direnv to common system packages

--- modules/common-system.nix
+++ modules/common-system.nix
@@ -39,7 +39,7 @@
   # --- PAQUETES ESENCIALES (SOLO CLI) --- 
   environment.systemPackages = with pkgs; [
     git wget curl vim btop htop fastfetch pciutils lshw usbutils dnsutils openssl zip unzip fish
-    ripgrep fd jq bat tree
+    ripgrep fd jq bat tree direnv
   ];
 
   system.stateVersion = "25.11";
