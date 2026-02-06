9ea598b8d2911a6acda8a140c46bde8da7fc3355
juanqp07 <odioelmando2@gmail.com>
2026-02-06 17:51:51 +0100

    Añadido protonplus

--- modules/desktop-gaming.nix
+++ modules/desktop-gaming.nix
@@ -80,7 +80,7 @@
     heroic rustdesk-flutter
     go lm_sensors obs-studio gcc
     syncthing jetbrains.idea openjdk25
-    freecad feishin
+    freecad feishin protonplus
   ];
 
   programs.nix-ld.enable = true;
