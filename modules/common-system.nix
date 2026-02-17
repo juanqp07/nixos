f18d3178a6a1fdc93bf6675ea4a0a640dbc89a2c
JuanQ <87898162+juanqp07@users.noreply.github.com>
2026-02-17 12:15:05 +0100

    Update common-system.nix

--- modules/common-system.nix
+++ modules/common-system.nix
@@ -4,7 +4,40 @@
   # --- BOOT ---
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  boot.kernel.sysctl = {
+      # --- Rendimiento de Red ---
+      "net.core.default_qdisc" = "cake";
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.core.rmem_max" = 16777216;
+      "net.core.wmem_max" = 16777216;
+      "net.ipv4.tcp_rmem" = "4096 87380 16777216";
+      "net.ipv4.tcp_wmem" = "4096 65536 16777216";
+      "net.ipv4.tcp_tw_reuse" = 1;
+  
+      # --- Seguridad de Red (AJUSTADO PARA DOCKER) ---
+      "net.ipv6.conf.all.forwarding" = 0;
+      "net.ipv4.icmp_echo_ignore_all" = 1;
+      "net.ipv4.conf.all.rp_filter" = 0;
+      "net.ipv4.conf.default.rp_filter" = 0;
+  
+      "net.ipv4.conf.all.accept_redirects" = 0;
+      "net.ipv6.conf.all.accept_redirects" = 0;
+      "net.ipv4.conf.default.accept_redirects" = 0;
+      "net.ipv6.conf.default.accept_redirects" = 0;
+      "net.ipv4.conf.all.accept_source_route" = 0;
+      "net.ipv6.conf.all.accept_source_route" = 0;
+      "net.ipv4.conf.default.accept_source_route" = 0;
+      "net.ipv6.conf.default.accept_source_route" = 0;
+  
+      # --- Memoria y Ficheros ---
+      "fs.file-max" = 2097152;
+      "fs.nr_open" = 1048576;
+      "vm.swappiness" = 10;
+      "vm.max_map_count" = 262144;
+  
+      # --- Miscelánea de Seguridad ---
+      "kernel.sysrq" = 0;
+    };
   # --- RED Y LOCALIZACIÓN ---
   networking.networkmanager.enable = true;
   time.timeZone = "Europe/Madrid";
