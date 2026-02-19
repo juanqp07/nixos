2041894478e361629830ec6c7717b424f603182d
JuanQ <87898162+juanqp07@users.noreply.github.com>
2026-02-19 11:24:51 +0100

    Update common-system.nix

--- modules/common-system.nix
+++ modules/common-system.nix
@@ -1,90 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  # --- BOOT ---
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernel.sysctl = {
-      # Reiniciar automáticamente 10 segundos después de un Kernel Panic
-      "kernel.panic" = 10;
-      
-      # Reiniciar si hay un "Oops" (fallo interno del kernel)
-      "kernel.panic_on_oops" = 1;
-    
-      # Reiniciar si el sistema se queda sin memoria (OOM) en lugar de intentar matar procesos
-      "vm.panic_on_oom" = 1;
-    
-      # Reiniciar si se detecta un bloqueo de software (soft lockup)
-      "kernel.softlockup_panic" = 1;
-
-      # --- Rendimiento de Red ---
-      "net.core.default_qdisc" = "cake";
-      "net.ipv4.tcp_congestion_control" = "bbr";
-      "net.core.rmem_max" = 16777216;
-      "net.core.wmem_max" = 16777216;
-      "net.ipv4.tcp_rmem" = "4096 87380 16777216";
-      "net.ipv4.tcp_wmem" = "4096 65536 16777216";
-      "net.ipv4.tcp_tw_reuse" = 1;
-  
-      # --- Seguridad de Red (AJUSTADO PARA DOCKER) ---
-      "net.ipv6.conf.all.forwarding" = 0;
-      "net.ipv4.icmp_echo_ignore_all" = 1;
-      "net.ipv4.conf.all.rp_filter" = 0;
-      "net.ipv4.conf.default.rp_filter" = 0;
-  
-      "net.ipv4.conf.all.accept_redirects" = 0;
-      "net.ipv6.conf.all.accept_redirects" = 0;
-      "net.ipv4.conf.default.accept_redirects" = 0;
-      "net.ipv6.conf.default.accept_redirects" = 0;
-      "net.ipv4.conf.all.accept_source_route" = 0;
-      "net.ipv6.conf.all.accept_source_route" = 0;
-      "net.ipv4.conf.default.accept_source_route" = 0;
-      "net.ipv6.conf.default.accept_source_route" = 0;
-  
-      # --- Memoria y Ficheros ---
-      "fs.file-max" = 2097152;
-      "fs.nr_open" = 1048576;
-      "vm.swappiness" = 10;
-      "vm.max_map_count" = 262144;
-  
-      # --- Miscelánea de Seguridad ---
-      "kernel.sysrq" = 0;
-    };
-  # --- RED Y LOCALIZACIÓN ---
-  networking.networkmanager.enable = true;
-  time.timeZone = "Europe/Madrid";
-  
-  i18n.defaultLocale = "es_ES.UTF-8";
-
-  console.keyMap = lib.mkDefault "es";
-
-  # --- SERVICIOS DE RED ---
-  services.netbird.enable = true;
-
-  # --- CONFIGURACIÓN DE USUARIO ---
-  users.users.juan = {
-    isNormalUser = true;
-    description = "juan";
-    shell = pkgs.fish;
-    extraGroups = [ "networkmanager" "wheel" ]; 
-  };
-  programs.fish.enable = true;
-
-  # --- NIX SETTINGS ---
-  nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d"; 
-  };
-  nix.settings.auto-optimise-store = true;
-  services.fwupd.enable = true;
-  # --- PAQUETES ESENCIALES (SOLO CLI) --- 
-{ config, pkgs, lib, ... }:
-
-{
   # --- 1. BOOT Y RESILIENCIA DEL KERNEL ---
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -188,7 +104,4 @@
     nix-up = "pushd ~/nixos && nix flake update && sudo nixos-rebuild switch --flake .#$(hostname) && sudo fwupdmgr update && popd";
     nix-clean = "sudo nix-collect-garbage -d && nix-store --optimize";
   };
-}
-  };
-
 }
