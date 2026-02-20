81035394a87a52e286c8ac59335a5f799817d5c5
juanqp07 <odioelmando2@gmail.com>
2026-02-20 22:48:45 +0100

    Añadido plymouth

--- modules/desktop-gaming.nix
+++ modules/desktop-gaming.nix
@@ -3,9 +3,20 @@
 {
   # --- KERNEL ---
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
+  boot.kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
   services.cloudflare-warp.enable = true;
+  boot.plymouth.enable = true;
 
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
   # --- ENTORNO GRÁFICO (Plasma 6) ---
   services.xserver.enable = true;
   services.xserver.xkb = { layout = "es"; variant = ""; };
