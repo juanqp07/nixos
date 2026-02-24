c0ae94106338c1443b55288c1de801bae2502774
JuanQ <87898162+juanqp07@users.noreply.github.com>
2026-02-25 00:39:28 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-flatpak.url = "github:gmodena/nix-flatpak";
+    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
   };
 
   outputs = { self, nixpkgs, nix-flatpak, ... }@inputs: {
